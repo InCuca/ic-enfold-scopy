7fe6a4885b49ee23e58f12bc61b6aef0bd462d93
Walker <walker@incuca.com.br>
2019-05-15 16:37:36 -0300

    fix script path

--- ic-enfold-scopy.php
+++ ic-enfold-scopy.php
@@ -15,7 +15,7 @@ $plugin_dir = plugin_dir_url(__FILE__);
 
 $loadScripts = function() use (&$plugin_dir) {
     wp_localize_script( 'ic-scopy', 'ajaxUrl', admin_url('admin-ajax.php') );
-    wp_enqueue_script( 'ic-scopy', "$plugin_dir/js/ic-scopy.js");
+    wp_enqueue_script( 'ic-scopy', "${plugin_dir}js/ic-scopy.js");
 };
 
 add_action( 'wp_enqueue_scripts', $loadScripts );
