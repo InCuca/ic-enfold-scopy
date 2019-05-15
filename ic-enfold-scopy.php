17efc9273e4b48d5711e50bd6fb3bcb98cc1de34
Walker <walker@incuca.com.br>
2019-05-15 18:17:32 -0300

    add script

--- ic-enfold-scopy.php
+++ ic-enfold-scopy.php
@@ -14,8 +14,27 @@
 $plugin_dir = plugin_dir_url(__FILE__);
 
 $loadScripts = function() use (&$plugin_dir) {
-    wp_localize_script( 'ic-scopy', 'ajaxUrl', admin_url('admin-ajax.php') );
-    wp_enqueue_script( 'ic-scopy', "${plugin_dir}js/ic-scopy.js");
+    global $post;
+    
+    wp_register_script( 'ic-scopy', "${plugin_dir}js/ic-scopy.js");
+    $icScopy = array(
+        'url' => admin_url('admin-ajax.php', $protocol),
+        'params' => array(
+            'action' => 'ic_scopy_shortcode',
+            '_nonce' => wp_create_nonce('ic-scopy-nonce'),
+            'postId' => $post->ID,
+        ),
+    );
+    wp_localize_script( 'ic-scopy', 'ic_scopy', $icScopy );
+    wp_enqueue_script('ic-scopy');
 };
-
 add_action( 'wp_enqueue_scripts', $loadScripts );
+
+$scopyShortcode = function() {
+    check_ajax_referer( 'ic-scopy-nonce', '_nonce' );
+    $postId = sanitize_text_field($_REQUEST['postId']);
+    echo get_post_meta( $postId, '_aviaLayoutBuilderCleanData', true );
+    die();
+};
+add_action('wp_ajax_ic_scopy_shortcode', $scopyShortcode);
+add_action('wp_ajax_nopriv_ic_scopy_shortcode', $scopyShortcode);
--- js/ic-scopy.js
+++ js/ic-scopy.js
@@ -1,10 +1,71 @@
-'use strict'
+function fallbackCopyTextToClipboard(text) {
+    const textArea = document.createElement("textarea");
+    textArea.value = text;
+    document.body.appendChild(textArea);
+    textArea.focus();
+    textArea.select();
 
-const copier = document.createElement('div');
-div.innerHTML = 'Copiar Shortcode';
-div.style = {
-    position: 'fixed',
-    background: 'black',
-    color: 'white'
-};
-document.appendChild(copier);
+    try {
+        const successful = document.execCommand('copy');
+        const msg = successful ? 'successful' : 'unsuccessful';
+        console.log('Fallback: Copying text command was ' + msg);
+    } catch (err) {
+        console.error('Fallback: Oops, unable to copy', err);
+    }
+
+    document.body.removeChild(textArea);
+}
+
+function copyTextToClipboard(text) {
+    if (!navigator.clipboard) {
+        fallbackCopyTextToClipboard(text);
+        return;
+    }
+    navigator.clipboard.writeText(text).then(function () {
+        window.alert('Copiado para o clipboard');
+    }, function (err) {
+        window.alert('Erro ao copiar');
+        console.error('Async: Could not copy text: ', err);
+    });
+}
+
+function getAsUriParameters(data) {
+    let url = '';
+    for (const prop in data) {
+        url += encodeURIComponent(prop) + '=' +
+            encodeURIComponent(data[prop]) + '&';
+    }
+    return url.substring(0, url.length - 1)
+}
+
+function onShortcodeLoaded(evt) {
+    const res = evt.target.responseText;
+    copyTextToClipboard(res);
+}
+
+function loadShortcode() {
+    const {url, params} = window.ic_scopy;
+    const oReq = new XMLHttpRequest();
+    oReq.onload = onShortcodeLoaded;
+    oReq.open("GET", `${url}?${getAsUriParameters(params)}`, true);
+    oReq.setRequestHeader("Content-Type", "application/json");
+    oReq.send();
+}
+
+function setupIcScopy() {
+    const a = document.createElement('a');
+    a.innerHTML = 'Copiar Shortcode';
+    a.style.position = 'fixed';
+    a.style.bottom = '0';
+    a.style.left = '0';
+    a.style.zIndex = '9999';
+    a.style.background = 'black';
+    a.style.color = 'white';
+    a.href = '#';
+
+    a.addEventListener('click', loadShortcode);
+    
+    document.body.appendChild(a);
+}
+
+document.addEventListener('DOMContentLoaded', setupIcScopy);
